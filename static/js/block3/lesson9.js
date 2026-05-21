b7e98c8f07868cd646292239036b082f0af34846
Alexander Rudenko <learnitnow@yandex.ru>
2026-05-21 14:20:36 +0200

    core

--- static/js/block3/lesson9.js
+++ static/js/block3/lesson9.js
@@ -46,6 +46,13 @@ define(function () {
  * 
  */
 
+    function debounce(func, ms) {
+        let timeout;
+        return function() {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => func.apply(this, arguments), ms);
+        };
+    }
 
     };
 });
